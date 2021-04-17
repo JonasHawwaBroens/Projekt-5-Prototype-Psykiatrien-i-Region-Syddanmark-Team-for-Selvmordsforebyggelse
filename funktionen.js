eb2d41b6582985d5da9aa7346118ff412cbb5ae0
Jonas Hawwa Brøns <70684189+JonasHawwaBroens@users.noreply.github.com>
2021-04-17 19:09:59 +0200

    Kode-opdatering, d. 17/4 2021

--- funktionen.js
+++ funktionen.js
@@ -5,3 +5,18 @@ function openMenu() {
 function closeMenu() {
     document.getElementById("myMenu").style.height = "0%"
 }
+
+const GTInput = document.querySelectorAll(".gt-input");
+
+for (let i = 0; i < GTInput.length; i++) {
+    let currentLabel = GTInput[i]
+    .parentElement.firstElementChild;
+
+    GTInput[i].addEventListener("focus", function() {
+        currentLabel.classList.add("move-up");
+    })
+
+    GTInput[i].addEventListener("blur", function() {
+        currentLabel.classList.remove("move-up");
+    })
+}
